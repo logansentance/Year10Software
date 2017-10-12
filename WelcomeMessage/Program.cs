fe824c569269253fbe72886f81e696c89b4e0477
Logan Sentance <Logan.Sentance@education.nsw.gov.au>
2017-10-12 12:39:41 +1100

    made hello world and welcome message

--- WelcomeMessage/Program.cs
+++ WelcomeMessage/Program.cs
@@ -10,10 +10,12 @@ namespace WelcomeMessage
     {
         static void Main(string[] args)
         {
+            Console.Write("write your age: ");
+            string age = Console.ReadLine();
             Console.Write("Write you name: ");
             string name = Console.ReadLine();
             Console.WriteLine($"Im {name} and this is my Pawn shop");
-
+            Console.WriteLine($"What is your {Age} ");
 
 
 
