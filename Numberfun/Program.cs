86c9d2fe7b43ba8cef3e721de19240c518abd230
Logan Sentance <Logan.Sentance@education.nsw.gov.au>
2017-10-18 09:48:33 +1100

    number

---
+++ Numberfun/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numberfun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Inputs
+            Console.WriteLine("Pleasse enter a number for X:");
+            float X = float.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter a number for Y:");
+            float Y = float.Parse(Console.ReadLine());
+
+
+            //Output
+            Console.WriteLine("X plus Y = " + (X + Y));
+            Console.WriteLine("X minus Y = " + (X - Y));
+            Console.WriteLine("X times Y = " + (X * Y));
+            Console.WriteLine("X divided Y = " + (X / Y));
+
+            //Wait
+            Console.ReadKey();
+        }
+    }
+}
