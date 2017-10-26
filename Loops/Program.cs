9bb3e0d2de95079a40c9c64642365f3528949442
Logan Sentance <Logan.Sentance@education.nsw.gov.au>
2017-10-26 12:39:41 +1100

    Loops

---
+++ Loops/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // wait
+            Console.WriteLine("Please enter to start");
+            Console.ReadLine();
+
+            for(int i = 1; i <= 69; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            //While loop
+            int j = 1;
+            while(j <= 69)
+            {
+                Console.WriteLine("j");
+                j++;
+            }
+
+
+            // wait
+            Console.ReadKey();
+        }
+    }
+}
