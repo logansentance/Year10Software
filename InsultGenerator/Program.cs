fc39240da705516fbdcddf59991cb83340bbb3b2
Logan Sentance <Logan.Sentance@education.nsw.gov.au>
2017-11-09 12:19:54 +1100

    Insult Generator

--- InsultGenerator/Program.cs
+++ InsultGenerator/Program.cs
@@ -23,28 +23,33 @@ namespace InsultGenerator
             Console.WriteLine("Welcome to the insult generator. What is your name?");
             return Console.ReadLine();
         }
-        private static void GenrateInsult(string name, Random rnd)
+        private static void GenerateInsult(string name, Random rnd)
         {
-            Console.WriteLine(name + ", " + GetAdjective(rnd) + " " + GetCompoundAdjective(rnd) + " " + GetObject(rnd));
+            Console.WriteLine("");
+            Console.WriteLine(name + ", you are " + GetAdjective(rnd) + " " + GetCompoundAdjective(rnd) + " " + GetObject(rnd));
             Menu(name, rnd);
         }
         private static void Menu(string name, Random rnd)
         {
+            Console.WriteLine("Would you like another insult? y/n");
+            if (Console.ReadLine() == "y")
 
+                GenerateInsult(name, rnd);
         }
-        private static string GetAdjective()
+        private static string GetAdjective(Random rnd)
         {
             string[] adjectives = { "a Transgender", "a Nigga-Fagot", "Crusty ass", "Transage", "Pedo" };
-            return adjectives[rnd.Next(8)];
+            return adjectives[rnd.Next(5)];
         }
-        private static string GetCompundAdjective()
+        private static string GetCompoundAdjective(Random rnd)
         {
             string[] compoundAdjectives = { "Nigga-Faggot", "SJW", "Micro-Penised", "Lizard-Man", "Ricegum-Dickrider", "Dickrider", "Irelevent", "Communist"};
             return compoundAdjectives[rnd.Next(8)];
         }
-        private static string GetObject()
+        private static string GetObject(Random rnd)
         {
             string[] objects = { "Gnome", "Sheep", "Pig-Doggo", "Retard", "Attack-Helicopter", "Turtle", "Nigga-Fagot" };
+            return objects[rnd.Next(7)];
         }
     }
 }
